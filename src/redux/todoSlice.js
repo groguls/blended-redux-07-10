7b7aa8d2a601fc4d9c226c8cdc0396bc5ff44be8
groguls <groguls@gmail.com>
2023-10-07 10:48:34 +0200

    part2 - create redux store

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const initialState = { todos: [] };
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: {
+      reducer: (state, action) => {
+        state.todos.push(action.payload);
+      },
+      prepare: text => {
+        const id = nanoid();
+        return { payload: { id, text } };
+      },
+    },
+    deleteTodo(state, action) {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+export const todoReducer = todoSlice.reducer;
